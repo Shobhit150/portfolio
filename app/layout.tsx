7d8204d917ca07c869012a01f0b3079b8fc3b9b8
Shobhit150 <vshobhit150@gmail.com>
2025-07-10 15:27:15 +0530

    fix: ico and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Navbar from "@/components/navbar"
 import { cn } from "@/lib/utils";
-
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter as FontSans } from "next/font/google";
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -24,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
@@ -36,6 +44,8 @@ export default function RootLayout({
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
